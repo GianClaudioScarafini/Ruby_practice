13a19db1c3e0293ee674ef5f372051584c871a82
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2025-12-30 17:48:58 +0000

    fix the indetaion and rubocop

--- 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,6 +1,6 @@
 class OrangeTree
   # TODO: Implement all the specs defined in the README.md :)
-  attr_accessor :age, :dead, :height
+  attr_accessor :age, :dead, :height, :fruits
 
   def initialize
     @age = 0
@@ -10,22 +10,30 @@ class OrangeTree
   end
 
   def one_year_passes!
-    @age += 1
-    @height += 1 if @age <= 10
-    @dead = true if @age > 100
-
-    if @age <= 5
-      @fruits = 0
-    elsif @age > 5 && @age < 10
-      @fruits = 100
-    elsif @age >= 10 && @age < 15
-      @fruits = 200
-    else
-      @fruits = 0
+    unless dead?
+      grow!
+      may_die!
+      produce_fruits!
     end
   end
 
+  attr_reader :fruits
+
   def dead?
+    @dead
+  end
+
+  def pick_a_fruit!
+    if @fruits <= 0
+      0
+    else
+      @fruits -= 1
+    end
+  end
+
+  private
+
+  def may_die!
     if @age > 100
       @dead = true
     elsif @age > 50
@@ -36,27 +44,20 @@ class OrangeTree
     end
   end
 
-  attr_reader :height, :fruits, :age
+  def grow!
+    @age += 1
+    @height += 1 if @age <= 10
+  end
 
-  def pick_a_fruit!
-    if @fruits <= 0
-      0
+  def produce_fruits!
+    if @age <= 5
+      @fruits = 0
+    elsif @age > 5 && @age < 10
+      @fruits = 100
+    elsif @age >= 10 && @age < 15
+      @fruits = 200
     else
-      @fruits -= 1
+      @fruits = 0
     end
   end
 end
-
-# mytree = OrangeTree.new
-# 7.times do
-#   mytree.one_year_passes!
-# end
-# p mytree.fruits ##100
-# p mytree.age
-
-# p"----"
-# 5.times do
-#   mytree.one_year_passes!
-# end
-# p mytree.fruits ##200
-# p mytree.age ##12
