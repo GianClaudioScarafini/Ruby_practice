cc10717e6d2c8d96c39f1dfe307e11645e2a64ac
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2026-01-03 20:53:04 +0000

    flow array

--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
@@ -5,7 +5,18 @@ require_relative "wagon_sort"
 #       - `gets`:  https://ruby-doc.org/3.3.5/Kernel.html#method-i-gets
 #       - `chomp`: https://ruby-doc.org/3.3.5/String.html#method-i-chomp
 
+3.times do
+  name = gets.chomp
+  @students ||= []
+  @students << name
+end
 
-
+if @students.empty?
+  puts "The wagon has no students."
+else
+  sorted_students = wagon_sort(@students)
+  ## "The wagon students are: Alice, charlie, daniel\n" to match /Alice, Bob, charlie and daniel/
+  puts "The wagon students are: #{sorted_students[0..-2].join(', ')} and #{sorted_students[-1]}."
+end
 # TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
 #       file and display the sorted student list
--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,8 @@
 def wagon_sort(students)
   # TODO: return (not print!) a copy of students, sorted alphabetically
+  students.sort_by { |name| name.downcase }
 end
+
+
+# p wagon_sort(["BOB", "ALICE", "CHARLIE"]) # ["ALICE", "BOB", "CHARLIE"]
+# p wagon_sort([]) # ["ALICE", "BOB", "CHARLIE"]
