e142416c876b46442819579d394807ccc1523e52
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2026-01-04 09:38:12 +0000

    Proof Of Concept finsih methods

--- 01-Ruby/02-Flow-Array/04-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Flow-Array/04-Black-Jack/lib/croupier.rb
@@ -2,8 +2,23 @@ require_relative 'black_jack'
 
 def state_of_the_game(player_score, bank_score)
   # TODO: return (not print!) a message containing the player's score and bank's score
+  return "Your score is #{player_score}, bank is #{bank_score}"
 end
 
 def end_game_message(player_score, bank_score)
   # TODO: return (not print!) a message telling if the user won or lost.
+  if player_score > 21 
+    "Lost"
+  elsif player_score == 21
+    "Black Jack"
+  elsif player_score > bank_score
+    "Win"
+  elsif player_score < bank_score
+    "Lose"
+  else
+    "Push"
+  end
 end
+
+p end_game_message(18, 18)
+p end_game_message(22, 16)
