72f81720b1faab9ead83aabd59a3775c50456c20
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2026-01-05 12:34:13 +0000

    add splitter first part

--- 01-Ruby/03-Iterators-Blocks/05-Splitter/lib/splitter.rb
+++ 01-Ruby/03-Iterators-Blocks/05-Splitter/lib/splitter.rb
@@ -2,6 +2,14 @@ def size_splitter(array, size)
   # TODO: Return an array of two arrays, the first containing
   #       words of length `size`, the second containing all the other words
   #       In addition to this split, each array should be *sorted*.
+  first_array = []
+  second_array = []
+  array.each do |element|
+    first_array << element if element.length == size
+    second_array  << element if element.length != size
+    
+  end
+  return[first_array.sort!,second_array.sort!]
 end
 
 def block_splitter(array)
