94adbe1f5f1df11cd1c26cac2b30274e86eb01e5
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2026-01-10 14:26:59 +0000

    array to hash with block

--- 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,19 @@
 def array_to_hash(array)
   # TODO: implement the method :)
+  my_hash = {}
+  if block_given?
+    array.each_with_index do |element, i|
+      my_hash["#{yield i}"] = element
+    end
+  else
+    array.each_with_index do |element, i|
+      my_hash["#{i}"] = element
+    end
+
+  end
+
+  return my_hash
 end
+
+
+p array_to_hash(["array", "casa"])
