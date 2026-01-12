31a099e66b7cb94bb99f7f6461894852e89280a8
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2026-01-12 09:03:51 +0000

    box_office fun to not reflect the specs

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -1,5 +1,14 @@
 require 'csv'
+require 'date'
 
+file_path = "lib/movies.csv"
+p file_path
 def most_successful(number, max_year, file_path)
   # TODO: return an array of most successful movies before `max_year`
+  list = []
+  CSV.foreach(file_path) do |row|
+    list << { "name": row[0], "year": row[1], "earnings": row[2] } if row[1].to_i <= max_year 
+  end
+  return list.first(number)
 end
+p most_successful(5,2000,file_path)
